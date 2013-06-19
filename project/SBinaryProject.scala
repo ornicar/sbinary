781cb1de5ccd128fbea70eef050bf2492305996d
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-06-19 16:06:11 +0200

    publish to scala.iliaz.com

--- project/SBinaryProject.scala
+++ project/SBinaryProject.scala
@@ -1,52 +1,54 @@
-  import sbt._
-  import Keys._
+import sbt._, Keys._
 
-object SBinaryProject extends Build
-{
-	lazy val root = Project("root", file(".")) settings( aux("SBinary Parent") : _*) aggregate(core, treeExample)
-	lazy val core = Project("core", file("core")) settings(coreSettings : _*)
-	lazy val treeExample = Project("examples", file("examples") / "bt") settings( aux("SBinary Tree Example") : _*) dependsOn(core)
+object SBinaryProject extends Build {
+  lazy val root = Project("root", file(".")) settings (aux("SBinary Parent"): _*) aggregate (core, treeExample)
+  lazy val core = Project("core", file("core")) settings (coreSettings: _*)
+  lazy val treeExample = Project("examples", file("examples") / "bt") settings (aux("SBinary Tree Example"): _*) dependsOn (core)
 
-	lazy val commonSettings: Seq[Setting[_]] = Seq(
-		organization := "org.scala-tools.sbinary",
-		version := "0.4.1-THIB",
-		scalaVersion := "2.11.0-M3"
-	)
+  lazy val commonSettings: Seq[Setting[_]] = Seq(
+    organization := "org.scala-tools.sbinary",
+    version := "0.4.1-THIB",
+    scalaVersion := "2.11.0-M3"
+  )
 
-	lazy val scalaCheck = libraryDependencies += "org.scalacheck" % "scalacheck_2.11.0-M3" % "1.10.1" % "test"
-	lazy val coreSettings = commonSettings ++ template ++ Seq(
-		name := "SBinary",
-		scalaCheck,
-		unmanagedResources in Compile <+= baseDirectory map { _ / "LICENSE" }
-	)
-	def aux(nameString: String) = commonSettings ++ Seq( publish := (), name := nameString )
+  lazy val scalaCheck = libraryDependencies += "org.scalacheck" % "scalacheck_2.11.0-M3" % "1.10.1" % "test"
+  lazy val coreSettings = commonSettings ++ template ++ Seq(
+    name := "SBinary",
+    scalaCheck,
+    unmanagedResources in Compile <+= baseDirectory map { _ / "LICENSE" },
+    publishTo := Some(Resolver.sftp(
+      "iliaz",
+      "scala.iliaz.com"
+    ) as ("scala_iliaz_com", Path.userHome / ".ssh" / "id_rsa"))
+  )
+  def aux(nameString: String) = commonSettings ++ Seq(publish := (), name := nameString)
 
-	/*** Templating **/
+  /*** Templating **/
 
-	lazy val fmpp = TaskKey[Seq[File]]("fmpp")
-	lazy val fmppOptions = SettingKey[Seq[String]]("fmpp-options")
-	lazy val fmppConfig = config("fmpp") hide
+  lazy val fmpp = TaskKey[Seq[File]]("fmpp")
+  lazy val fmppOptions = SettingKey[Seq[String]]("fmpp-options")
+  lazy val fmppConfig = config("fmpp") hide
 
-	lazy val template = fmppConfig(Test) ++ fmppConfig(Compile) ++ templateBase
-	lazy val templateBase = Seq(
-		libraryDependencies += "net.sourceforge.fmpp" % "fmpp" % "0.9.14" % fmppConfig.name,
-		ivyConfigurations += fmppConfig,
-		fmppOptions := "--ignore-temporary-files" :: Nil,
-		fullClasspath in fmppConfig <<= update map { _ select configurationFilter(fmppConfig.name) map Attributed.blank }
-	)
-		
-	def fmppConfig(c: Configuration): Seq[Setting[_]] = inConfig(c)(Seq(
-		sourceGenerators <+= fmpp.identity,
-		fmpp <<= fmppTask,
-		scalaSource <<= (baseDirectory, configuration) { (base,c) => base / (Defaults.prefix(c.name) + "src") },
-		mappings in packageSrc <<= (managedSources, sourceManaged) map { (srcs, base) => srcs x relativeTo(base) },
-		sources <<= managedSources.identity
-	))
-	lazy val fmppTask =
-		(fullClasspath in fmppConfig, runner in fmpp, unmanagedSources, scalaSource, sourceManaged, fmppOptions, streams) map { (cp, r, sources, srcRoot, output, args, s) =>
-			IO.delete(output)
-			val arguments = "-U" +: "all" +: "-S" +: srcRoot.getAbsolutePath +: "-O" +: output.getAbsolutePath +: (args ++ sources.getPaths)
-			toError(r.run("fmpp.tools.CommandLine", cp.files, arguments, s.log))
-			(output ** "*.scala").get
-		}
+  lazy val template = fmppConfig(Test) ++ fmppConfig(Compile) ++ templateBase
+  lazy val templateBase = Seq(
+    libraryDependencies += "net.sourceforge.fmpp" % "fmpp" % "0.9.14" % fmppConfig.name,
+    ivyConfigurations += fmppConfig,
+    fmppOptions := "--ignore-temporary-files" :: Nil,
+    fullClasspath in fmppConfig <<= update map { _ select configurationFilter(fmppConfig.name) map Attributed.blank }
+  )
+
+  def fmppConfig(c: Configuration): Seq[Setting[_]] = inConfig(c)(Seq(
+    sourceGenerators <+= fmpp.identity,
+    fmpp <<= fmppTask,
+    scalaSource <<= (baseDirectory, configuration) { (base, c) ⇒ base / (Defaults.prefix(c.name) + "src") },
+    mappings in packageSrc <<= (managedSources, sourceManaged) map { (srcs, base) ⇒ srcs x relativeTo(base) },
+    sources <<= managedSources.identity
+  ))
+  lazy val fmppTask =
+    (fullClasspath in fmppConfig, runner in fmpp, unmanagedSources, scalaSource, sourceManaged, fmppOptions, streams) map { (cp, r, sources, srcRoot, output, args, s) ⇒
+      IO.delete(output)
+      val arguments = "-U" +: "all" +: "-S" +: srcRoot.getAbsolutePath +: "-O" +: output.getAbsolutePath +: (args ++ sources.getPaths)
+      toError(r.run("fmpp.tools.CommandLine", cp.files, arguments, s.log))
+      (output ** "*.scala").get
+    }
 }
